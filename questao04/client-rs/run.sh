3d1cdb088aca10fb47d2a773aca8dc6665b05b68
Rodrigo Bento <rodrigobentor2014@gmail.com>
2018-04-20 23:35:17 -0300

    Questao 4 corrigida

--- questao04/client-rs/run.sh
+++
@@ -1,6 +0,0 @@
-#mvn clean install
-sudo docker stop client
-sudo docker rm client
-sudo docker rmi questao4/client
-sudo docker build -t questao4/client .
-sudo docker run -p 8085:8080 -d --name client questao4/client
